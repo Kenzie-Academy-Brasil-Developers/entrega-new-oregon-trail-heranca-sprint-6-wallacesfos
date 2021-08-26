69fbd6aa4f93d2804718b031580812c44b81a2c4
Wallace <73260587+wallacesfos@users.noreply.github.com>
2021-08-26 12:26:47 -0300

    Feeeeeeeeeeeeeito

--- features/step_definitions/HunterSteps.js
+++ features/step_definitions/HunterSteps.js
@@ -12,9 +12,10 @@ Given('um Hunter de nome {string}', function (string) {
 When('o Hunter sair para caçar {int} vezes', function (int) {
     // When('o Hunter sair para caçar {float} vezes', function (float) {
       // Write code here that turns the phrase above into concrete actions
-      for(let i = 0; i < int; i++){
+    for(let i = 0; i < int; i++){
         hunter.hunt()
     }
+    console.log(hunter)
 });
 
 When('o Hunter parar para comer {int} vezes', function (int) {
@@ -25,6 +26,12 @@ When('o Hunter parar para comer {int} vezes', function (int) {
     }
 });
 
+Then('a quantidade de refeições do Hunter deve ser igual a {int}', function (int) {
+    // Then('a quantidade de refeições do Hunter deve ser igual a {float}', function (float) {
+      // Write code here that turns the phrase above into concrete actions
+    assert.strictEqual(hunter.food, int);
+});
+
 
 Then('o Hunter não ficará doente', function () {
     // Write code here that turns the phrase above into concrete actions
@@ -33,5 +40,5 @@ Then('o Hunter não ficará doente', function () {
 
 Then('o Hunter ficará doente', function () {
     // Write code here that turns the phrase above into concrete actions
-    assert.strictEqual(!hunter, false)
+    assert.strictEqual(hunter.isHealthy, false)
 });
